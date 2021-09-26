18308b0e38640fc13cffa19b3ee069dfff423206
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2021-09-26 17:17:36 +0600

    add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className="header-container">
+            <h1>BengalDroid Software Team</h1>
+            <p>Be the legend in your way to make products</p>
+            <h2>Total Budget This Year: $3200000M</h2>
+        </div>
+    );
+};
+
+export default Header;
