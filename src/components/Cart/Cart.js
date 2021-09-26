f3354c4cf50c412afcc94d42ab6febbe6fc51c32
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2021-09-26 23:24:33 +0600

    add code

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,13 +4,19 @@ const Cart = (props) => {
     const {cart} = props;
     let total = 0;
     for(const engineer of cart){
+        
         total += engineer.salary;
     }
     return (
         <div className="container">
             <h3>Engineers</h3>
             <h5>Engineers Total: {props.cart.length}</h5>
-            <h5>Engineers Name: {props.cart.name}</h5>
+            <ul>
+                {
+                    props.cart.map(engineer=>
+                        <ol>Name:{engineer.name}</ol>)
+                }
+            </ul>
             <br />
             <p>Salary: $ {total.toFixed(2)}</p>
         </div>
