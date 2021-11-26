1e4b16a2aab5eca95be6f58ec48876cbd7e27b9d
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2021-11-26 18:19:00 +0600

    add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div className="header-container">
             <h1>BengalDroid Software Team</h1>
             <p>Be the legend in your way to make products</p>
-            <h2>Total Budget This Year: $320M</h2>
+            <h2>Total Budget This Year: $325M</h2>
         </div>
     );
 };
